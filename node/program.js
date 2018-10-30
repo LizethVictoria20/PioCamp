878589c56a9cbd5cfb8e9ad45ed40b6725a1694e
Lizeth Victoria Franco <lizvictoria754@gmail.com>
2018-10-30 12:44:30 -0500

    Mi primer I/O

--- node/program.js
+++ node/program.js
@@ -1,5 +1,8 @@
-var suma = 0;
-for(var i = 2; i < process.argv.length; i++){
-  suma = suma +  Number(process.argv[i]);
-}
-  console.log(suma)
+var fs = require('fs'); //Para cargar un modulo
+var ruta = process.argv[2]; // La ruta que va tomar es el segundo argumento que da process.argv
+var read = fs.readFileSync(ruta); //Pasa ese modulo a un buffer
+var str = read.toString(); //Pasar ese buffer a string
+var array = str.split('\n') //Va a separar por salto de linea
+var saltoDeLinea = array.length - 1; //No va a contar el ultimo salto de linea
+
+console.log(saltoDeLinea);
